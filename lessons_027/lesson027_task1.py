1dd6f61a7f4202f835aa08944adf3ee95f854a0f
Igor <igor.gerasimov22@gmail.com>
2026-07-08 14:27:24 +0300

    add lessons 27 task 1

--- lessons_027/lesson027_task1.py
+++ lessons_027/lesson027_task1.py
@@ -1,12 +1,28 @@
 def print_header():
     print("=" * 30)
 
+def load_inventory():
+    inventory_file_name = input("Enter inventory file name: ")
+    servers_inventory = []
+    with open(inventory_file_name, "r") as file:
+        for server in file:
+            server = server.strip()
+            servers_inventory.append(server)
+    return servers_inventory
+    
+def print_servers(servers):
+    for server in servers:
+        print(server)
+
 def main():
     print_header()
-    pri
-
-def itr():
-    pri
+    try:
+        server_inventory_list = load_inventory()
+        print_servers(server_inventory_list)
+    except FileNotFoundError:
+        print("File not found")
+        print("Script terminated")
+    print_header()
 
 if __name__ == "__main__":
     main()
