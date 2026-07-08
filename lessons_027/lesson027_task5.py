4bc90c3f4de918984626eeb5803d09a67438c39d
igor <igor.gerasimov22@gmail.com>
2026-07-08 19:39:12 +0300

    add lesson 027 task 5

--- lessons_027/lesson027_task5.py
+++ lessons_027/lesson027_task5.py
@@ -10,12 +10,25 @@ def load_inventory_file():
 
 def parse_servers_inventory(servers_inventory):
     parsed_servers_inventory = []
+    empty_lines = 0
     for line in servers_inventory:
         server = line.strip()
         if not server:
+            empty_lines += 1
             continue
         parsed_servers_inventory.append(server)
-    return parsed_servers_inventory
+    return parsed_servers_inventory, empty_lines
+
+def find_duplicates_servers(parsed_servers_inventory):
+    uniq_servers = set()
+    duplicate_servers = set()
+    for server in parsed_servers_inventory:
+        if server in uniq_servers:
+            duplicate_servers.add(server)
+        else:
+            uniq_servers.add(server)
+    return uniq_servers, duplicate_servers
+
 
 def print_servers(servers):
     for server in servers:
@@ -23,9 +36,20 @@ def print_servers(servers):
 
 def main():
     print_header()
-    servers_inventory = load_inventory_file()
-    parsed_servers_inventory = parse_servers_inventory(servers_inventory)
-    print_servers(parsed_servers_inventory)
+    try:
+        servers_inventory = load_inventory_file()
+        parsed_servers_inventory, empty_lines = parse_servers_inventory(servers_inventory)
+        uniq_servers, duplicate_servers = find_duplicates_servers(parsed_servers_inventory)
+        print("INVENTORY REPORT\n")
+        print(f"Total records: {len(parsed_servers_inventory)}")
+        print(f"Unique servers: {len(uniq_servers)}")
+        print(f"Duplicate servers: {len(duplicate_servers)}")
+        print(f"Empty lines: {empty_lines}\n")
+        print("Duplicate servers:")
+        print_servers(duplicate_servers)
+    except FileNotFoundError:
+        print("ERROR")
+        print("File not found")
     print_header()
 
 if __name__ == "__main__":
