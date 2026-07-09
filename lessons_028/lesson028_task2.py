60c21d38d0b08496868aefedfc958bbce494a772
igor <igor.gerasimov22@gmail.com>
2026-07-09 16:43:46 +0300

    add lesson 028 task 2

---
+++ lessons_028/lesson028_task2.py
@@ -0,0 +1,51 @@
+def print_header():
+    print("=" * 30)
+
+def load_passwd_file():
+    passwd_file = []
+    with open("/etc/passwd", "r") as file:
+        for line in file:
+            passwd_file.append(line)
+    return passwd_file
+
+def parse_passwd_file(passwd_file):
+    parsed_passwd_file = []
+    for line in passwd_file:
+        line = line.strip()
+        if not line:
+            continue
+        user = line.split(':')
+        parsed_passwd_file.append(
+            {
+                "username": user[0],
+                "password": user[1],
+                "uid": user[2],
+                "gid": user[3],
+                "description": user[4],
+                "home": user[5],
+                "shell": user[6]
+            }
+        )
+    return parsed_passwd_file
+
+def print_users(parsed_passwd_file):
+    for line in parsed_passwd_file:
+        if line['shell'] == "/bin/bash":
+            print(f"username: {line['username']}\tshell: {line['shell']}")
+
+def main():
+    print_header()
+    try:
+        passwd_file = load_passwd_file()
+        parsed_passwd_file =parse_passwd_file(passwd_file)
+        print_users(parsed_passwd_file)
+    except FileNotFoundError:
+        print("ERROR")
+        print("File not found")
+    except PermissionError:
+        print("ERROR")
+        print("Access deniaed")
+    print_header()
+
+if __name__ == "__main__":
+    main()
